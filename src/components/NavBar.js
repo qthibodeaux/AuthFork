f658bf1800082b1f3a7013befa3bc86e43778816
qthibodeaux <60246734+qthibodeaux@users.noreply.github.com>
2023-12-19 22:18:46 -0600

    updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { useAuth } from '../useAuth'
+import supabaseClient from '../supabaseClient'
 
 function NavBar() {
+  const { sess } = useAuth()
+
   return (
-    <div>NavBar</div>
+    <div className='NavBar'>
+      <h4>NavBar</h4>
+      {sess 
+        ? <div>true</div>
+        : <div>false</div>
+      }
+    </div>
   )
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { Auth } from '@supabase/auth-ui-react'
+import supabaseClient from '../supabaseClient'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../useAuth'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 
-function Register() {
+export const setReturnPath = () => {
+  localStorage.setItem("returnPath", window.location.pathname)
+}
+
+function Register () {
+  const [authMode, setAuthMode] = useState("sign_in")
+  const { sess } = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (sess?.user) {
+      navigate("../")
+    }
+  }, [sess])
+
   return (
-    <div>Register</div>
+    <section>
+      <div>
+        <Auth 
+          supabaseClient={supabaseClient}
+          appearance={{ 
+            theme: ThemeSupa
+          }}
+          view={authMode}
+        />
+      </div>
+    </section>
   )
 }
 
