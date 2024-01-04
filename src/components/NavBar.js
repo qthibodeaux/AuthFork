10eeb7931234ac2af6ac4ba84e08e8aa8b27af2d
qthibodeaux <60246734+qthibodeaux@users.noreply.github.com>
2024-01-03 23:02:49 -0600

    Changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,48 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../useAuth'
 import supabaseClient from '../supabaseClient'
 
+function RegisterButton () {
+  let navigate = useNavigate()
+  
+  return (
+      <button 
+          type="button" 
+          className="btn"
+          id="navbutton"
+          onClick={() => {
+              navigate('/register')
+          }}
+      >
+          <span className='nav-dev'>register</span>
+      </button>
+  )
+}
+
+function LogoutButton () {
+  let navigate = useNavigate()
+  const { logout } = useAuth()
+  const handleLogout = () => {
+      navigate('/login')
+      logout()
+  }
+
+  return (
+      <button type='button' className='btn ' id="navbutton" onClick={handleLogout}><span className='nav-dev'>log</span>out</button>
+  )
+}
+
 function NavBar() {
   const { sess } = useAuth()
+  let navigate = useNavigate()
+
 
   return (
     <div className='NavBar'>
       <h4>NavBar</h4>
       {sess 
-        ? <div>true</div>
-        : <div>false</div>
+        ? <RegisterButton />
+        : <LogoutButton />
       }
     </div>
   )
