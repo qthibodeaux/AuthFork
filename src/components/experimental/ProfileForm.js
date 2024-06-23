673d76f5df29a09fe0b351fa0469075f577dc635
qthibodeaux <60246734+qthibodeaux@users.noreply.github.com>
2024-06-22 23:48:46 -0500

    Update ProfileForm.js

--- src/components/experimental/ProfileForm.js
+++ src/components/experimental/ProfileForm.js
@@ -1,42 +1,59 @@
 import { useState } from 'react'
-import { Box, Button, Card, Form, FormField, Grid, Heading, Text, TextInput } from 'grommet'
+import { Box, Button, Card, Grid, Heading, Text, TextInput } from 'grommet'
 import { FormPreviousLink } from 'grommet-icons'
 
-function ProfileForm() {
-    const [formArray, setFormArray] = useState([])
-    const [formVariable, setFormVariable] = ('4')
-    const [mainAncestor, setMainAncestor] = useState(null)
+function ProfileForm () {
+    const [formVariable, setFormVariable] = useState('2')
+    const [mainAncestor, setMainAncestor] = useState("Mary")
     const [firstName, setFirstName] = useState("")
     const [lastName, setLastName] = useState("")
-    const [nickName, setNickName] = useState("")
+    const [nickName, setNickName] = useState("Lady")
     const [parent, setParent] = useState("")
     const [sunrise, setSunrise] = useState("")
     
+    const [activeTab, setActiveTab] = useState("FirstNameForm")
+
+    const MainPage = ({ activeTab }) => {
+        let comp = <h1 style={{backgroundColor: 'red', padding: '20px', fontWeight: 600, color: '#fff'}}>Developer Error: invalid tabId!</h1>
+        if (activeTab === 'AncestorForm') comp = <AncestorForm setMainAncestor={setMainAncestor} setFormVariable={setFormVariable} setActiveTab={setActiveTab}/>
+        else if (activeTab === 'NameForm') comp = <NameForm setMainAncestor={setMainAncestor} setFormVariable={setFormVariable} setActiveTab={setActiveTab} setFirstName={setFirstName} setNickName={setNickName} />
+        else if (activeTab === 'FirstNameForm') comp = <FirstNameForm setNickName={setNickName} setFormVariable={setFormVariable} setActiveTab={setActiveTab} setFirstName={setFirstName} />
+        else if (activeTab === 'LastNameForm') comp = <LastNameForm setLastName={setLastName} setFormVariable={setFormVariable} setActiveTab={setActiveTab} setFirstName={setFirstName} setNickName={setNickName} />
+        else if (activeTab === 'NickNameForm') comp = <NickNameForm setMainAncestor={setMainAncestor} setFormVariable={setFormVariable} setActiveTab={setActiveTab} setFirstName={setFirstName} />
+        else if (activeTab === 'ParentForm') comp = <ParentForm setMainAncestor={setMainAncestor} setFormVariable={setFormVariable} />
+        else if (activeTab === 'SunriseForm') comp = <SunriseForm setMainAncestor={setMainAncestor} setFormVariable={setFormVariable} />
+        
+        return (
+            <Box
+                fill
+                pad='small'
+            >{comp}</Box>
+        )
+    }
 
   return (
     <Box
         fill
-        gap='medium'
+        gap='small'
     >
-        <PeakerBox />
-        <MainBox progress={formVariable} />
-        <Box>
-            Todo: Components
-            PeakerButton
-            Side Progression Bar
-            Completed Items Box
-            QuestionBox
-        </Box>
-        
+        <PeakerBox mainAncestor={mainAncestor} />
+        <FormInfoBox firstName={firstName} nickName={nickName} lastName={lastName} parent={parent} sunrise={sunrise} />
+        <TopProgressionBox formVariable={formVariable}/>
+        <MainPage activeTab={activeTab} /> 
     </Box>
   )
 }
 
-const PeakerBox = () => {
+const ParentForm = () => {return <Box>PF</Box>}
+const SunriseForm = () => {return <Box>SRF</Box>}
+
+
+const PeakerBox = ({ mainAncestor }) => {
     return (
         <Box
             fill='horizontal'
             pad={{ horizontal: 'small'}}
+            style={{ visibility: mainAncestor ? 'visible' : 'hidden'}}
         >
             <Box
                 background='neutral-1'
@@ -44,37 +61,21 @@ const PeakerBox = () => {
                 round={{ size: 'small', corner: 'bottom' }}
                 pad='medium'
                 fill='horizontal'
+                animation={{ type: 'fadeIn' }}
             >
-                <Box
-                    animation={{ type: 'fadeIn' }}
-                                
-                >
-                    <Heading level='2'>House of Mary</Heading>
-                    <Heading level='3'>4th Branch</Heading>
-                </Box>
+                {mainAncestor === 'Friend of Family' ? <Heading level='2' textAlign='center'>{mainAncestor}</Heading> : <Heading level='2' textAlign='center'>House of {mainAncestor}</Heading>}
+                <Heading level='3' textAlign='center'>4th Branch</Heading>  
             </Box>
         </Box>
     )
 }
 
-const MainBox = ({ progress }) => {
-    return (
-        <Box
-            fill
-            gap='medium'
-        >
-            <FormInfoBox />
-            <TopProgressionBox progress={progress}/>
-            <ContentBox />
-        </Box>
-    )
-}
-
-const FormInfoBox = () => {
+const FormInfoBox = ({ firstName, lastName, nickName, parent, sunrise}) => {
     return (
         <Box
             fill='horizontal'
             pad='small'
+            style={{ visibility: firstName || nickName ? 'visible' : 'hidden'}}
         >
             <Box
                 fill='horizontal'
@@ -83,15 +84,16 @@ const FormInfoBox = () => {
                 pad='medium'
             >
                 <Box direction='column' gap='medium'>
-                    <Box direction='row'>
-                        <Heading>Quintus</Heading>
-                        <Heading> Thibodeaux</Heading>
+                    <Box direction='row' align='center' justify='center' gap='medium'>
+                        <Heading >{firstName}</Heading>
+                        {nickName && <Heading >{nickName}</Heading>}
+                        <Heading >{lastName}</Heading>
                     </Box>
-                    <Box direction='row'>
-                        <Heading>Son of JoAnn</Heading>
+                    <Box direction='row' align='center' justify='center'>
+                        {parent && <Heading >Child of {parent}</Heading>}
                     </Box>
-                    <Box direction='row'>
-                        <Heading>April 16 1985</Heading>
+                    <Box direction='row' align='center' justify='center'>
+                        <Heading >{sunrise}</Heading>
                     </Box>
                 </Box>
             </Box>
@@ -99,7 +101,7 @@ const FormInfoBox = () => {
     )
 }
 
-const TopProgressionBox = ({ progress }) => {
+const TopProgressionBox = ({ formVariable }) => {
     const heights = {
         0: { box1: '1rem' },
         1: { box1: '25%' },
@@ -116,8 +118,8 @@ const TopProgressionBox = ({ progress }) => {
         4: { col1: 'neutral-1' },
     }
     //blank red orange yellow green
-    const { box1 } = heights[progress]
-    const { col1 } = colors[progress]
+    const { box1 } = heights[formVariable]
+    const { col1 } = colors[formVariable]
 
 
     return (
@@ -134,24 +136,14 @@ const TopProgressionBox = ({ progress }) => {
     )
 }
 
-const ContentBox = () => {
-    const setMainAncestor = () => {}
-    const [comp, setComp] = useState(<NameForm setMainAncestor={setMainAncestor} />)
-    return (
-        <Box
-            fill
-            pad='small'
-        >
-            {comp}
-        </Box>
-    )
-}
-
-
-
-const AncestorForm = ({setMainAncestor}) => {
-    const cards = ["Sylvestor", "Jack", "Ben", "John", "Loretta", "Hazel", "Bobbie", "Joyce", "Lorene", "Alma"]
-        .map((name, i) => <Text key={i}>{name}</Text>)
+const AncestorForm = ({ setMainAncestor, setFormVariable, setActiveTab }) => {
+    let textsize = 'xsmall'
+    const names = ["Sylvestor", "Jack", "Ben", "John", "Loretta", "Hazel", "Bobbie", "Joyce", "Lorene", "Alma", "Friend of Family"]
+    const cards = names.map((name, i) => {
+        if (name === "Friend of Family") return <Text key={i} size={textsize}>{name}</Text>
+        return <Text key={i}>{name}</Text>
+    })
+        
     return (
         <Box
             fill
@@ -166,22 +158,50 @@ const AncestorForm = ({setMainAncestor}) => {
                 columns='xsmall'
                 gap='small'
             >
-                {cards.map((card, index) => (
-                    <Card
-                        background='black'
-                        pad='large'
-                        key={index}
-                    >
-                        {card}
-                    </Card>
-                ))}
+                {names.map((name, index) => {
+                    return (
+                        <Card
+                            background='black'
+                            pad='large'
+                            key={index}
+                            onClick={() => {
+                                setMainAncestor(name)
+                                setFormVariable('1')
+                                setActiveTab('NameForm')
+                            }}
+                        >
+                            {cards[index]}
+                        </Card>)
+                    }
+                )}
                 
             </Grid>
         </Box>
     )
 }
 
-const NameForm = () => {
+const NameForm = ({ setMainAncestor, setFormVariable, setActiveTab, setFirstName, setNickName } ) => {
+    const [value, setValue] = useState('')
+    const [showError, setShowError] = useState(false)
+    const onChange = (event) => {
+        if (value === '') setShowError(true)
+        else setShowError(false)
+        setValue(event.target.value)
+    }
+
+    const checkValidFirstName = () => {
+        if (value === '') return setShowError(true) 
+        setFormVariable('2')
+        setActiveTab('NickNameForm')
+        setFirstName(value)
+    }
+
+    const checkValidNickName = () => {
+        if (value === '') return setShowError(true) 
+        setFormVariable('2')
+        setActiveTab('FirstNameForm')
+        setNickName(value)
+    }
 
     return (
         <Box
@@ -199,7 +219,8 @@ const NameForm = () => {
                 <Box
                     gap='medium'
                 >
-                    <TextInput />
+                    <TextInput onChange={onChange}/>
+                    <Box style={{ visibility: showError ? 'visible' : 'hidden' }}><Text color='red' weight='bold'>Required</Text></Box>
                     <Box
                         direction='row'
                         justify='center'
@@ -209,6 +230,7 @@ const NameForm = () => {
                             round='small'
                             background='light-1'
                             pad='medium'
+                            onClick={() => checkValidFirstName()}
                         >
                             <Text
                                 weight='bold'
@@ -221,6 +243,7 @@ const NameForm = () => {
                             round='small'
                             background='light-1'
                             pad='medium'
+                            onClick={() => checkValidNickName()}
                         >
                             <Text
                                 weight='bold'
@@ -231,36 +254,214 @@ const NameForm = () => {
                         </Box> 
                     </Box>
                 </Box>
+                
             </Box>
-            <Box pad='large'>
+            <Box
+                pad='large'
+                onClick={() => {
+                    setMainAncestor('')
+                    setActiveTab('AncestorForm')
+                    setFormVariable('0')
+                }}
+            >
                 <Text textAlign='center' weight='bold' size='large'>Back</Text>
             </Box>
         </Box>
     )
 }
 
-const FormTemplate = () => {
+const FirstNameForm = ({ setNickName, setFormVariable, setActiveTab, setFirstName }) => {
+    const [value, setValue] = useState('')
+    const onChange = (event) => setValue(event.target.value)
+
     return (
         <Box
             fill
+            round='small'
             alignContent='center'
-            pad='large'
+            pad='medium'
+            background='neutral-1'
+            justify='between'
         >
-            <Heading >Who yo mama nem?</Heading>
-            <TextInput />
-            <Box>
+            <Box
+                gap='medium'
+            >
+                <Heading textAlign='center'>What is your first name?</Heading>
                 <Box
-                    justify='center'
-                    align='center'
-                    width='3rem'
-                    height='3rem'
-                    background='neutral-1'
-                    round='small'
+                    gap='medium'
                 >
-                    <Button fill icon={<FormPreviousLink />} />
+                    <TextInput onChange={onChange}/>
+                    <Box
+                        direction='row'
+                        justify='center'
+                        gap='medium'
+                    >
+                        <Box
+                            round='small'
+                            background='light-1'
+                            pad='medium'
+                            onClick={() => {
+                                setFormVariable('3')
+                                setActiveTab('LastNameForm')
+                                setFirstName(value)
+                            }}
+                        >
+                            <Text
+                                weight='bold'
+                                size='large'
+                            >
+                                Submit
+                            </Text>
+                        </Box> 
+                    </Box>
                 </Box>
-                <Button label='Back'/>
-                <Button label='Submit' primary/>
+            </Box>
+            <Box
+                pad='large'
+                onClick={() => {
+                    setNickName('')
+                    setFormVariable('1')
+                    setActiveTab('NameForm')
+                }}
+            >
+                <Text textAlign='center' weight='bold' size='large'>Back</Text>
+            </Box>
+        </Box>
+    )
+}
+
+const NickNameForm = ({ setNickName, setFormVariable, setActiveTab, setFirstName }) => {
+    const [value, setValue] = useState('')
+    const onChange = (event) => setValue(event.target.value)
+
+    return (
+        <Box
+            fill
+            round='small'
+            alignContent='center'
+            pad='medium'
+            background='neutral-1'
+            justify='between'
+        >
+            <Box
+                gap='medium'
+            >
+                <Heading textAlign='center'>Do you have a nickname?</Heading>
+                <Box
+                    gap='medium'
+                >
+                    <TextInput onChange={onChange}/>
+                    <Box
+                        direction='row'
+                        justify='center'
+                        gap='medium'
+                    >
+                        <Box
+                            round='small'
+                            background='light-1'
+                            pad='medium'
+                            onClick={() => {
+                                setFormVariable('3')
+                                setActiveTab('LastNameForm')
+                            }}
+                        >
+                            <Text
+                                weight='bold'
+                                size='large'
+                            >
+                                No
+                            </Text>
+                        </Box>
+                        <Box
+                            round='small'
+                            background='light-1'
+                            pad='medium'
+                            onClick={() => {
+                                setFormVariable('3')
+                                setActiveTab('LastNameForm')
+                                setNickName(value)
+                            }}
+                        >
+                            <Text
+                                weight='bold'
+                                size='large'
+                            >
+                                Submit
+                            </Text>
+                        </Box> 
+                    </Box>
+                </Box>
+            </Box>
+            <Box
+                pad='large'
+                onClick={() => {
+                    setFirstName('')
+                    setFormVariable('1')
+                    setActiveTab('NameForm')
+                }}
+            >
+                <Text textAlign='center' weight='bold' size='large'>Back</Text>
+            </Box>
+        </Box>
+    )
+}
+
+const LastNameForm = ({ setLastName, setFormVariable, setActiveTab, setFirstName, setNickName }) => {
+    const [value, setValue] = useState('')
+    const onChange = (event) => setValue(event.target.value)
+
+    return (
+        <Box
+            fill
+            round='small'
+            alignContent='center'
+            pad='medium'
+            background='neutral-1'
+            justify='between'
+        >
+            <Box
+                gap='medium'
+            >
+                <Heading textAlign='center'>What is your last name?</Heading>
+                <Box
+                    gap='medium'
+                >
+                    <TextInput onChange={onChange}/>
+                    <Box
+                        direction='row'
+                        justify='center'
+                        gap='medium'
+                    >
+                        <Box
+                            round='small'
+                            background='light-1'
+                            pad='medium'
+                            onClick={() => {
+                                setFormVariable('4')
+                                setActiveTab('SunriseForm')
+                                setLastName(value)
+                            }}
+                        >
+                            <Text
+                                weight='bold'
+                                size='large'
+                            >
+                                Submit
+                            </Text>
+                        </Box> 
+                    </Box>
+                </Box>
+            </Box>
+            <Box
+                pad='large'
+                onClick={() => {
+                    setFirstName('')
+                    setNickName('')
+                    setFormVariable('1')
+                    setActiveTab('NameForm')
+                }}
+            >
+                <Text textAlign='center' weight='bold' size='large'>Back</Text>
             </Box>
         </Box>
     )
