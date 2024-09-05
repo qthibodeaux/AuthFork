9488dc1ee082712ab61325f57d1203c7295ac8b4
qthibodeaux <60246734+qthibodeaux@users.noreply.github.com>
2024-09-04 23:42:13 -0500

    Converstion to ant moble

--- src/components/experimental/ScrollTree.js
+++ src/components/experimental/ScrollTree.js
@@ -1,48 +1,49 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
 import { Row, Col, Card, Typography, Image } from 'antd';
-import { supabase } from "../supabaseClient"
+import { supabase } from '../supabaseClient';
 
 function ScrollTree() {
-    const [profiles, setProfiles] = useState([]);
-    const [names, setNames] = useState([])
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const { Title, Text } = Typography;
+  const [profiles, setProfiles] = useState([]);
+  const [names, setNames] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { Title, Text } = Typography;
 
-    useEffect(() => {
-        const fetchProfiles = async () => {
-            try {
-                setLoading(true);
-                const { data, error } = await supabase
-                    .from('profile')
-                    .select('*')
-                    .order('sunrise', { ascending: true })
-                    
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      try {
+        setLoading(true);
+        const { data, error } = await supabase
+          .from('profile')
+          .select('*')
+          .order('sunrise', { ascending: true });
 
-                if (error) {
-                throw error;
-                }
-
-                setProfiles(data)
-            } catch (error) {
-                setError(error.message);
-              } finally {
-                setLoading(false);
-              }
+        if (error) {
+          throw error;
         }
 
-        fetchProfiles();
-    }, [])
+        setProfiles(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    const groupedProfiles = profiles.reduce((acc, profile) => {
-        (acc[profile.branch] = acc[profile.branch] || []).push(profile);
-        return acc;
-      }, {});
-    
-      // Sort profiles within each branch by sunrise date
-      Object.keys(groupedProfiles).forEach(branch => {
-        groupedProfiles[branch].sort((a, b) => new Date(a.sunrise) - new Date(b.sunrise));
-      });
+    fetchProfiles();
+  }, []);
+
+  const groupedProfiles = profiles.reduce((acc, profile) => {
+    (acc[profile.branch] = acc[profile.branch] || []).push(profile);
+    return acc;
+  }, {});
+
+  // Sort profiles within each branch by sunrise date
+  Object.keys(groupedProfiles).forEach((branch) => {
+    groupedProfiles[branch].sort(
+      (a, b) => new Date(a.sunrise) - new Date(b.sunrise)
+    );
+  });
 
   return (
     <Row justify="center" align="middle" style={{ padding: '16px' }}>
@@ -57,13 +58,17 @@ function ScrollTree() {
               <Card key={branch} style={{ marginBottom: '16px' }}>
                 <Title level={3}>Branch {branch}</Title>
                 <Row gutter={8} style={{ marginBottom: '8px' }}>
-                  {displayedProfiles.slice(0, 5).map(profile => (
+                  {displayedProfiles.slice(0, 5).map((profile) => (
                     <Col key={profile.id} style={{ padding: '8px' }}>
                       {profile.avatar_url ? (
                         <Image
                           src={profile.avatar_url}
                           alt={`${profile.firstname} ${profile.lastname}`}
-                          style={{ borderRadius: '8px', width: '50px', height: '50px' }}
+                          style={{
+                            borderRadius: '8px',
+                            width: '50px',
+                            height: '50px',
+                          }}
                         />
                       ) : (
                         <div
@@ -81,7 +86,7 @@ function ScrollTree() {
                 <Row gutter={8}>
                   {displayedProfiles.slice(5, 10).map((profile, index) => (
                     <Col key={profile.id} style={{ padding: '8px' }}>
-                      {index === 4 && additionalProfilesCount > 0 ? (
+                      {index === 3 && additionalProfilesCount > 0 ? (
                         <div
                           style={{
                             width: '50px',
@@ -99,7 +104,11 @@ function ScrollTree() {
                         <Image
                           src={profile.avatar_url}
                           alt={`${profile.firstname} ${profile.lastname}`}
-                          style={{ borderRadius: '8px', width: '50px', height: '50px' }}
+                          style={{
+                            borderRadius: '8px',
+                            width: '50px',
+                            height: '50px',
+                          }}
                         />
                       ) : (
                         <div
@@ -120,7 +129,7 @@ function ScrollTree() {
         </Card>
       </Col>
     </Row>
-  )
+  );
 }
 
-export default ScrollTree
+export default ScrollTree;
